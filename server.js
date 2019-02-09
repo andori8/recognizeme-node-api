4b308aeb083d76e6f4aa8539d67e5a38dde7fcd5
andori8 <falcsmike7@gmail.com>
2019-02-09 11:19:50 -0500

    added knex and connected to database

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'recognizeme'
+  }
+});
+
+db.select('*').from('users').then(data => {
+  console.log(data)
+});
 
 const app = express();
 
@@ -43,14 +58,16 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: '125',
-    name: name,
+  db('users')
+  .returning('*')
+  .insert({
     email: email,
-    entries: 0,
+    name: name,
     joined: new Date()
+  }).then(res => {
+    res.json(user[0])
   })
-  res.json(database.users[database.users.length-1])
+  .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
