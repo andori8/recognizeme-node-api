c679ce1fa302716a9383fe5b720ce100fc317030
andori8 <falcsmike7@gmail.com>
2019-02-11 14:28:45 -0500

    installed clarifai and moved api key to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: "92e006c3c0e44434b879b87e1866b239"
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+    res.json(data)
+  })
+  .catch(err => res.status(400).json("unable to work with API"))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +24,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
